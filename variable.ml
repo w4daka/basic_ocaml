a60c0fea6ad18f801bfc749898c1675598c669d8
w4daka <ignorenegativeideology999@gmail.com>
2026-02-14 12:49:30 +0900

    chapeter3 compleate

--- variable.ml
+++ variable.ml
@@ -1,2 +1,16 @@
 (25 * 850) + (28 * 850) + (31 * 850);;
-(25 + 28 + 31) * 850
+(25 + 28 + 31) * 850;;
+
+let pi = 3.1415;;
+
+pi;;
+2.0 *. pi;;
+
+let jikyu = 850;;
+
+(25 * jikyu) + (28 * jikyu) + (31 * jikyu);;
+
+let e = 2.7182
+let positive = true
+let second_of_day = 60 * 60 * 24
+let name = "茗荷谷"
