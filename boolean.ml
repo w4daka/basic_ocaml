28f79cd9d59999bf3e70fb4d3f3505d84dc6ce38
w4daka <ignorenegativeideology999@gmail.com>
2026-02-14 11:04:13 +0900

    boolean compleate

--- boolean.ml
+++ boolean.ml
@@ -5,4 +5,15 @@ true || false;;
 false || false;;
 not true;;
 not false;;
-not (false || ((not false) && not false))
+not (false || ((not false) && not false));;
+2 < 3;;
+2 < 3 && 2.0 >= 3.0;;
+2 < 3 && 2.0 = 3.0;;
+not (3 < 2);;
+true > false;;
+false > true;;
+true > true;;
+2 < 3;;
+not (3.1415 ** 2.0 > 10.0);;
+8 mod 3 = 2;;
+3 + 4 + 5 = 4 * 3
