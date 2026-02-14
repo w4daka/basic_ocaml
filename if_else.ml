13aa8c9f6fbe428f783cc17432b6ea87165cc27f
w4daka <ignorenegativeideology999@gmail.com>
2026-02-14 18:47:10 +0900

    if_else add

---
+++ if_else.ml
@@ -0,0 +1,7 @@
+if true then 1 else 2;;
+if false then 1 else 2;;
+if 1 < 2 then 3 else 4;;
+if 3 + 4 < 5 then 1 else 2;;
+if 2 < 1 then 3 else 4;;
+if "a" = "b" then false else true;;
+if not (3 = 4) then 1 < 2 else 1 > 2
--- kyuyo.ml
+++ kyuyo.ml
@@ -1,12 +1,15 @@
 (*時給(円)*)
 let jikyu = 950
 
+(*優遇時給(円)*)
+let yugu_jikyu = 980
+
 (*基本給(円)*)
 let kihonkyu = 100
 
 (*目的：働いた時間xに応じたアルバイト代を計算する*)
 (*kyuyo : int -> int *)
-let kyuyo x = kihonkyu + (jikyu * x)
+let kyuyo x = kihonkyu + if x < 30 then jikyu * x else x * yugu_jikyu
 let test1 = kyuyo 25 = 23850
 let test2 = kyuyo 28 = 26700
-let test1 = kyuyo 31 = 29550
+let test1 = kyuyo 31 = 30480
